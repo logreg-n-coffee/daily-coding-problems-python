7da4792d897875c46c87d7ea7327d2885f86e9e4
Rui Hu <8199515+logreg-n-coffee@users.noreply.github.com>
2023-08-10 09:52:40 -0700

    Add typing to Q 294

--- question_294.py
+++ question_294.py
@@ -65,9 +65,9 @@ def get_distances(edges: DefaultDict[int, List[Tuple[int, int]]], stack: List[in
 
 
 def shortest_route(elevations: Dict[int, int], paths: Dict[Tuple[Any, Any], int]) -> float:
-    uphill_edges = defaultdict(list)
-    downhill_edges = defaultdict(list)
-    all_edges = defaultdict(list)
+    uphill_edges: DefaultDict[int, List[Tuple[int, int]]] = defaultdict(list)
+    downhill_edges: DefaultDict[int, List[Tuple[int, int]]] = defaultdict(list)
+    all_edges: DefaultDict[int, List[Tuple[int, int]]] = defaultdict(list)
 
     for (start, end), distance in paths.items():
         all_edges[start].append((end, distance))
@@ -76,18 +76,19 @@ def shortest_route(elevations: Dict[int, int], paths: Dict[Tuple[Any, Any], int]
         else:
             downhill_edges[end].append((start, distance))
 
-    num_vertices = len(all_edges.keys())
-    stack = toposort(all_edges, num_vertices)
+    num_vertices: int = len(all_edges.keys())
+    stack: List[int] = toposort(all_edges, num_vertices)
 
-    uphill_distances = get_distances(uphill_edges, list(stack))
-    downhill_distances = get_distances(downhill_edges, list(stack))
+    uphill_distances: List[float] = get_distances(uphill_edges, list(stack))
+    downhill_distances: List[float] = get_distances(
+        downhill_edges, list(stack))
 
     return min(x + y for x, y in zip(uphill_distances, downhill_distances))
 
 
 if __name__ == '__main__':
-    elevations = {0: 5, 1: 25, 2: 15, 3: 20, 4: 10}
-    paths = {
+    elevations: Dict[int, int] = {0: 5, 1: 25, 2: 15, 3: 20, 4: 10}
+    paths: Dict[Tuple[int, int], int] = {
         (0, 1): 10,
         (0, 2): 8,
         (0, 3): 15,
